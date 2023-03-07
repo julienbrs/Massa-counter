274576f2fd3efa7cde15bca54e2dfec8b1c80263
julienbrs <julien.bourseau@gmail.com>
2023-03-07 13:00:54 +0100

    trigger_value ok

--- backend/assembly/contracts/main.ts
+++ backend/assembly/contracts/main.ts
@@ -1,5 +1,5 @@
 // The entry file of your WebAssembly module.
-import { Storage } from '@massalabs/massa-as-sdk';
+import { Storage, generateEvent } from '@massalabs/massa-as-sdk';
 import { Args, stringToBytes, fromBytes } from '@massalabs/as-types';
 
 export function increment(_args: StaticArray<u8>): void {
@@ -21,4 +21,20 @@ export function increment(_args: StaticArray<u8>): void {
     stringToBytes('counter'),
     new Args().add<u32>(newValue).serialize(),
   );
+}
+
+export function triggerValue(): void {
+  const counterArgs = new Args().add('counter');
+  const counterValue: u32 = fromBytes<u32>(
+    Storage.get(stringToBytes('counter')),
+  );
+  /* Choix d'implémentation à expliquer */
+  if (!Storage.has(counterArgs)) {
+    Storage.set(stringToBytes('counter'), new Args().add<u32>(0).serialize());
+  }
+  // else if (storeValue.isErr()) {
+  //   // initialiser à 0 ou retourner une erreur ?
+  // }
+
+  generateEvent(counterValue.toString());
 }
